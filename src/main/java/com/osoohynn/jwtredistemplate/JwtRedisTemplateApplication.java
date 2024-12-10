42b3ffe379df1950b9c4aa48341bed651e109162
su080915 <tbr335@naver.com>
2024-12-10 17:46:11 +0900

    initial commit

---
+++ src/main/java/com/osoohynn/jwtredistemplate/JwtRedisTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.osoohynn.jwtredistemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtRedisTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtRedisTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/osoohynn/jwtredistemplate/JwtRedisTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osoohynn.jwtredistemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtRedisTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
